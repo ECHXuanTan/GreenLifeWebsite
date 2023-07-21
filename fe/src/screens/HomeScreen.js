c08ee038ecbf0bd0759b8a1727ea124c67e00496
ECHXuanTan <93677722+ECHXuanTan@users.noreply.github.com>
2023-07-21 16:57:07 +0700

    Manage State by Reducer

--- fe/src/screens/HomeScreen.js
+++ fe/src/screens/HomeScreen.js
@@ -1,39 +1,69 @@
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 
 function HomeScreen() {
-    const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
     useEffect(() => {
       const fetchData = async () => {
+
+         dispatch({ type: 'FETCH_REQUEST' });
+      try {
         const result = await axios.get('/api/products');
-        setProducts(result.data);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
       };
       fetchData();
     }, []);
 
   return (
     <div>
-              <h1>SẢNG PHẨM MỚI</h1>
-        <div className="products">
-            {products.map((product) => (
+       <h1>Sản phẩm mới</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
             <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
-              </a>
+              </Link>
               <div className="product-info">
-                <a href={`/product/${product.slug}`}>
+                <Link to={`/product/${product.slug}`}>
                   <p>{product.name}</p>
-                </a>
+                </Link>
                 <p>
                   <strong>{product.price}đ</strong>
                 </p>
                 <button>Thêm vào giỏ hàng</button>
               </div>
             </div>
-          ))}
-        </div>  
+          ))
+        )}
+      </div> 
     </div>
   );
 }
