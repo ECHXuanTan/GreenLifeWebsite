cad9bf3bf52994b23556b0c26b6ae26f3c407dd6
ECHXuanTan <93677722+ECHXuanTan@users.noreply.github.com>
2023-07-23 00:45:15 +0700

    feature: Signin

--- be/routes/userRoutes.js
+++ be/routes/userRoutes.js
@@ -22,7 +22,7 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({ message: 'Invalid email or password' });
+    res.status(401).send({ message: 'Sai Email hoặc mật khẩu' });
   })
 );
 
