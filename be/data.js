a99f1ebd3729c020c46621a476bfb9a02f67af0a
ECHXuanTan <93677722+ECHXuanTan@users.noreply.github.com>
2023-07-22 18:44:38 +0700

    Signin Api

--- be/data.js
+++ be/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'admin1',
+      email: 'admin1@greenlife.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'admin2',
+      email: 'admin2@greenlife.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
     products: [
       {
         //_id: '1',
--- be/server.js
+++ be/server.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
 
 dotenv.config();
 
@@ -17,8 +19,13 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
